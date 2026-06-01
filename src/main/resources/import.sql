5475a7f25030cc01a810072452249ff8775081ed
franmaurizpereira <franmaurizpereira@yandex.com>
2026-06-01 10:49:59 +0200

    Creacion base de datos mariadb, tablka persona, configuracion conecxion springboot

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO persons (name, last_name, program_language) VALUES ('Andres', 'Guzman', 'Java');
+INSERT INTO persons (name, last_name, program_language) VALUES ('Pepe', 'Doe', 'Python');
+INSERT INTO persons (name, last_name, program_language) VALUES ('John', 'Dow', 'JavaScript');
+INSERT INTO persons (name, last_name, program_language) VALUES ('Maria', 'Roe', 'Java');
+INSERT INTO persons (name, last_name, program_language) VALUES ('Josefa', 'Rae', 'Java');
