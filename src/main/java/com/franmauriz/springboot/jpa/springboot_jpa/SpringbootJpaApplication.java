3b691609966e2e5af9bf6f03117cebea43ac9dca
franmaurizpereira <franmaurizpereira@yandex.com>
2026-06-02 11:03:33 +0200

    consultas personalizadas

--- src/main/java/com/franmauriz/springboot/jpa/springboot_jpa/SpringbootJpaApplication.java
+++ src/main/java/com/franmauriz/springboot/jpa/springboot_jpa/SpringbootJpaApplication.java
@@ -22,7 +22,11 @@ public class SpringbootJpaApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		
-		List<Person> persons = (List<Person>) repository.findAll();
+		// List<Person> persons = (List<Person>) repository.findAll();
+		//List<Person> persons = (List<Person>) repository.findByProgramLanguage("Java");
+		//List<Person> persons = (List<Person>) repository.buscarByProgramName("Andres");
+		//List<Person> persons = (List<Person>) repository.buscarByProgramLanguageAndName("Java", "Andres");
+		List<Person> persons = (List<Person>) repository.findByProgramLanguageAndName("Java", "Andres");
 
 		persons.stream().forEach(person -> System.out.println(person));
 	}
--- src/main/java/com/franmauriz/springboot/jpa/springboot_jpa/repositories/PersonRepository.java
+++ src/main/java/com/franmauriz/springboot/jpa/springboot_jpa/repositories/PersonRepository.java
@@ -1,9 +1,18 @@
 package com.franmauriz.springboot.jpa.springboot_jpa.repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.franmauriz.springboot.jpa.springboot_jpa.entities.Person;
+import java.util.List;
+
 
 public interface PersonRepository extends CrudRepository<Person,Long>{
+    List<Person> findByProgramLanguage(String programLanguage);
+    @Query("select p from  Person p where p.name = ?1")
+    List<Person> buscarByProgramName(String name);
+    @Query("select p from  Person p where p.programLanguage = ?1 and p.name = ?2")
+    List<Person> buscarByProgramLanguageAndName(String programLanguage, String name);
 
+    List<Person> findByProgramLanguageAndName(String programLanguage, String name);
 }
